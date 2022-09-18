03df11ff3467973faff97b50a42cba90e2f85458
hulx <hulx@mail.ustc.edu.cn>
2022-09-18 23:18:55 +0800

    hw4

--- src/hw4/Systems/CanvasSystem.cpp
+++ src/hw4/Systems/CanvasSystem.cpp
@@ -4,7 +4,13 @@
 
 #include <_deps/imgui/imgui.h>
 
+#include <Eigen/Dense>
+
+#include <math.h>
+
 using namespace Ubpa;
+using namespace std;
+using namespace Eigen;
 
 void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 	schedule.RegisterCommand([](Ubpa::UECS::World* w) {
@@ -16,6 +22,10 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 			ImGui::Checkbox("Enable grid", &data->opt_enable_grid);
 			ImGui::Checkbox("Enable context menu", &data->opt_enable_context_menu);
 			ImGui::Text("Mouse Left: drag to add lines,\nMouse Right: drag to scroll, click for context menu.");
+
+			static int e = 0;
+			ImGui::RadioButton("Stop fitting", &e, 0);
+			ImGui::RadioButton("Start fitting", &e, 1);
 
 			// Typically you would use a BeginChild()/EndChild() pair to benefit from a clipping region + own scrolling.
 			// Here we demonstrate that this can be replaced by simple offsetting + custom drawing + PushClipRect/PopClipRect() calls.
@@ -52,14 +62,104 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 			if (is_hovered && !data->adding_line && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
 			{
 				data->points.push_back(mouse_pos_in_canvas);
-				data->points.push_back(mouse_pos_in_canvas);
 				data->adding_line = true;
 			}
 			if (data->adding_line)
 			{
-				data->points.back() = mouse_pos_in_canvas;
 				if (!ImGui::IsMouseDown(ImGuiMouseButton_Left))
 					data->adding_line = false;
+			}
+
+			vector<float> t;
+			t.clear();
+			if (e > 0 && data->points.size() > 0)
+			{
+				float temp = 100.0f;
+				int n = data->points.size() - 1;
+				t.push_back(0.0f);
+				for (int i = 1; i <= n; i++)
+					t.push_back(t.back() + sqrt(pow(data->points[i][0] - data->points[i - 1][0], 2) + pow(data->points[i][1] - data->points[i - 1][1], 2)) / temp);
+			}
+
+			vector<pointf2> fitting_points;
+			fitting_points.clear();
+			if (e > 0 && data->points.size() == 2)
+			{
+				for (float t_iter = t[0]; t_iter <= t[1]; t_iter += 0.01f)
+				{
+					float x = data->points[1][0] / (t[1] - t[0]) * (t_iter - t[0]) + data->points[0][0] / (t[1] - t[0]) * (t[1] - t_iter);
+					float y = data->points[1][1] / (t[1] - t[0]) * (t_iter - t[0]) + data->points[0][1] / (t[1] - t[0]) * (t[1] - t_iter);
+					fitting_points.push_back(pointf2(x, y));
+				}
+			}
+			else if (e > 0 && data->points.size() > 2)
+			{
+				int n = data->points.size() - 1;
+				vector<float> h;
+				h.clear();
+				for (int i = 0; i <= n - 1; i++)
+					h.push_back(t[i + 1] - t[i]);
+				vector<float> u;
+				u.clear();
+				u.push_back(0.0f);
+				for (int i = 1; i <= n - 1; i++)
+					u.push_back(2 * (h[i] + h[i - 1]));
+				vector<float> b_x;
+				b_x.clear();
+				for (int i = 0; i <= n - 1; i++)
+					b_x.push_back(6 / h[i] * (data->points[i + 1][0] - data->points[i][0]));
+				vector<float> b_y;
+				b_y.clear();
+				for (int i = 0; i <= n - 1; i++)
+					b_y.push_back(6 / h[i] * (data->points[i + 1][1] - data->points[i][1]));
+				vector<float> v_x;
+				v_x.clear();
+				v_x.push_back(0.0f);
+				for (int i = 1; i <= n - 1; i++)
+					v_x.push_back(b_x[i] - b_x[i - 1]);
+				vector<float> v_y;
+				v_y.clear();
+				v_y.push_back(0.0f);
+				for (int i = 1; i <= n - 1; i++)
+					v_y.push_back(b_y[i] - b_y[i - 1]);
+				MatrixXf A = MatrixXf::Zero(n - 1, n - 1);
+				for (size_t i = 0; i < n - 1; i++)
+					A(i, i) = u[i + 1];
+				for (size_t i = 0; i < n - 2; i++)
+				{
+					A(i, i + 1) = h[i + 1];
+					A(i + 1, i) = h[i + 1];
+				}
+				MatrixXf V_x(n - 1, 1);
+				for (size_t i = 0; i < n - 1; i++)
+					V_x(i, 0) = v_x[i + 1];
+				MatrixXf V_y(n - 1, 1);
+				for (size_t i = 0; i < n - 1; i++)
+					V_y(i, 0) = v_y[i + 1];
+				MatrixXf M_x = A.colPivHouseholderQr().solve(V_x);
+				MatrixXf M_y = A.colPivHouseholderQr().solve(V_y);
+				for (int i = 0; i < n; i++)
+				{
+					for (float t_iter = t[i]; t_iter <= t[i + 1]; t_iter += 0.01f)
+					{
+						float x, y;
+						if (i == 0)
+						{
+							x = M_x(i, 0) / (6 * h[i]) * pow(t_iter - t[i], 3) + (data->points[i + 1][0] / h[i] - M_x(i, 0) * h[i] / 6) * (t_iter - t[i]) + data->points[i][0] / h[i] * (t[i + 1] - t_iter);
+							y = M_y(i, 0) / (6 * h[i]) * pow(t_iter - t[i], 3) + (data->points[i + 1][1] / h[i] - M_y(i, 0) * h[i] / 6) * (t_iter - t[i]) + data->points[i][1] / h[i] * (t[i + 1] - t_iter);
+						}
+						else if (i == n - 1)
+						{
+							x = M_x(i - 1, 0) / (6 * h[i]) * pow(t[i + 1] - t_iter, 3) + data->points[i + 1][0] / h[i] * (t_iter - t[i]) + (data->points[i][0] / h[i] - M_x(i - 1, 0) * h[i] / 6) * (t[i + 1] - t_iter);
+							y = M_y(i - 1, 0) / (6 * h[i]) * pow(t[i + 1] - t_iter, 3) + data->points[i + 1][1] / h[i] * (t_iter - t[i]) + (data->points[i][1] / h[i] - M_y(i - 1, 0) * h[i] / 6) * (t[i + 1] - t_iter);
+						}
+						else {
+							x = M_x(i - 1, 0) / (6 * h[i]) * pow(t[i + 1] - t_iter, 3) + M_x(i, 0) / (6 * h[i]) * pow(t_iter - t[i], 3) + (data->points[i + 1][0] / h[i] - M_x(i, 0) * h[i] / 6) * (t_iter - t[i]) + (data->points[i][0] / h[i] - M_x(i - 1, 0) * h[i] / 6) * (t[i + 1] - t_iter);
+							y = M_y(i - 1, 0) / (6 * h[i]) * pow(t[i + 1] - t_iter, 3) + M_y(i, 0) / (6 * h[i]) * pow(t_iter - t[i], 3) + (data->points[i + 1][1] / h[i] - M_y(i, 0) * h[i] / 6) * (t_iter - t[i]) + (data->points[i][1] / h[i] - M_y(i - 1, 0) * h[i] / 6) * (t[i + 1] - t_iter);
+						}
+						fitting_points.push_back(pointf2(x, y));
+					}
+				}
 			}
 
 			// Pan (we use a zero mouse threshold when there's no context menu)
@@ -80,7 +180,7 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 				if (data->adding_line)
 					data->points.resize(data->points.size() - 2);
 				data->adding_line = false;
-				if (ImGui::MenuItem("Remove one", NULL, false, data->points.size() > 0)) { data->points.resize(data->points.size() - 2); }
+				if (ImGui::MenuItem("Remove one", NULL, false, data->points.size() > 0)) { data->points.resize(data->points.size() - 1); }
 				if (ImGui::MenuItem("Remove all", NULL, false, data->points.size() > 0)) { data->points.clear(); }
 				ImGui::EndPopup();
 			}
@@ -95,8 +195,14 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 				for (float y = fmodf(data->scrolling[1], GRID_STEP); y < canvas_sz.y; y += GRID_STEP)
 					draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y), IM_COL32(200, 200, 200, 40));
 			}
-			for (int n = 0; n < data->points.size(); n += 2)
-				draw_list->AddLine(ImVec2(origin.x + data->points[n][0], origin.y + data->points[n][1]), ImVec2(origin.x + data->points[n + 1][0], origin.y + data->points[n + 1][1]), IM_COL32(255, 255, 0, 255), 2.0f);
+			// 标记原点
+			draw_list->AddCircle(ImVec2(origin.x, origin.y), 3, IM_COL32(255, 0, 0, 255), 0, 2.0f);
+			// 标记输入点集
+			for (int n = 0; n < data->points.size(); n++)
+				draw_list->AddCircle(ImVec2(origin.x + data->points[n][0], origin.y + data->points[n][1]), 3, IM_COL32(255, 255, 0, 255), 0, 2.0f);
+			// 连接采样点，绘制拟合函数图像
+			for (int n = 0; n + 1 < fitting_points.size(); n++)
+				draw_list->AddLine(ImVec2(origin.x + fitting_points[n][0], origin.y + fitting_points[n][1]), ImVec2(origin.x + fitting_points[n + 1][0], origin.y + fitting_points[n + 1][1]), IM_COL32(255, 255, 0, 255), 2.0f);
 			draw_list->PopClipRect();
 		}
 
